ea9511acc018d28e9c7cc4cc12715a5caf4fd7f4
utsav8165 <utsav8165@gmail.com>
2023-07-02 13:38:03 +0530

    day12 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:ucatalog/models/catalog.dart';
 import 'package:ucatalog/widgets/drawer.dart';
+import 'package:ucatalog/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
-  final days = 30;
-  final name = "utsav";
-
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text('Catalog App'),
       ),
-      body: Center(
-        child: Text("Welcome to $days days of Flutter by $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
+        ),
       ),
       drawer: MyDrawer(), // Use the MyDrawer widget here
     );
---
+++ lib/widgets/item_widget.dart
@@ -0,0 +1,58 @@
+import 'package:ucatalog/models/catalog.dart';
+import 'package:flutter/material.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+
+  const ItemWidget({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Card(
+        elevation: 8,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: ListTile(
+          onTap: () {
+            print("${item.name}");
+          },
+          contentPadding: EdgeInsets.all(16),
+          leading: ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.network(
+              item.image,
+              fit: BoxFit.cover,
+            ),
+          ),
+          title: Text(
+            item.name,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+          subtitle: Padding(
+            padding: EdgeInsets.only(top: 8),
+            child: Text(
+              item.desc,
+              style: TextStyle(
+                color: Colors.grey[600],
+              ),
+            ),
+          ),
+          trailing: Text(
+            "\$${item.price}",
+            textScaleFactor: 1.2,
+            style: TextStyle(
+              color: Colors.deepPurple,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
